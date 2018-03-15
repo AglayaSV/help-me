380266eb39386dc336e11dda6097112d6ee11a98
aglaya <aglayastunkus1@gmail.com>
2018-03-15 15:45:40 +0300

    solution3

--- src/index.js
+++ src/index.js
@@ -3,27 +3,29 @@ module.exports = function count(s, pairs) {
     for (let i = 0; i < pairs.length; i++) {
         N *= Math.pow(pairs[i][0], pairs[i][1]);
     }
-    if (N > 100000000){
-        if (pairs.length === 1){
+    if (N > 100000000) {
+        if (pairs.length === 1) {
 
-            if(goodTemplate()){
-            return pow(pairs[0][0], pairs[0][1]-1);
+            if (goodTemplate()) {
+                return pow(Math.pow(pairs[0][0], 9), (pairs[0][1] - 1) / 9);
             }
 
         }
         else return 0;
     }
+
     function pow(a, n) {
 
         let b = a;
         for (let i = 1; i < n; i++) {
-            b = (b*a)%1000000007;
+            b = (b * a) % 1000000007;
         }
         return b
     }
-function goodTemplate(){
+
+    function goodTemplate() {
         return true;
-}
+    }
 
     let bTemplate = s.split('');
     let res = 0;
@@ -45,8 +47,9 @@ function goodTemplate(){
         }
     }
 
-    return res%1000000007;
+    return res % 1000000007;
 };
+
 function NOD(A) {
     let n = A.length, x = Math.abs(A[0]);
     for (let i = 1; i < n; i++) {
